f95216644ff88a9a8c812ab9af283629c07f178b
Egno <ex.lxndr@gmail.com>
2018-12-06 21:00:12 +0700

    init

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container fluid fill-height>
+    <Login />
+  </v-container>
+</template>
+
+<script>
+import Login from "../components/Login";
+
+export default {
+  components: {
+    Login
+  }
+};
+</script>
