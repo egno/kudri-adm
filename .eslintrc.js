ff25eae8dae3410cc457a5d0a575daa840f8c016
Alexander Shelemetyev <ex.lxndr@gmail.com>
2018-12-16 09:11:55 +0300

    deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,35 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
+    "env": {
+      "browser": true,
+      "commonjs": true,
+      "es6": true,
+      "node": true
+    },
+    "parserOptions": {
+      "ecmaFeatures": {
+        "jsx": true
+      },
+      "sourceType": "module"
+    },
+    // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+    "extends":     ["eslint:recommended",
+    "plugin:vue/recommended"],
+    // required to lint *.vue files
+    "plugins": [
+      "vue"
+    ],
+    "rules": {
+      "import/no-unresolved": 0,
+      "import/no-unassigned-import": 0,
+      "semi": ["error", "never"],
+      "no-console": "off",
+      "space-before-function-paren": [
+        "error",
+        {
+          "anonymous": "always",
+          "named": "always",
+          "asyncArrow": "always"
+        }
+      ]
   }
 };
---
+++ src/components/UserAvatar.vue
@@ -0,0 +1,22 @@
+<template>
+  <VAvatar
+    class="ma-1"
+    :tile="(true)"
+  >
+    <img
+      src="/images/01.png"
+      alt="avatar"
+    >
+  </VAvatar> 
+</template>  
+
+<script>
+  export default {
+  data () {
+    return {
+      //
+    }
+  },
+  }
+</script>
+
