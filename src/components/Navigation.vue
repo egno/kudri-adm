61db0bd232ebc2e8e476e806c5e6130296cf7ced
Anton <sh09990@mail.ru>
2019-01-29 15:11:27 +0700

    menu navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,8 +1,9 @@
 <template>
   <VNavigationDrawer
+    disable-resize-watcher
     app
+    width="240"
     :dark="!isBusinessCard"
-    :value="navBarVisible"
     @input="onInput($event)"
   >
     <VToolbar flat>
@@ -10,25 +11,41 @@
         overflow-hidden
         @click="goHome()"
       >
-        {{ appTitle }}
+        <img
+          class="logo"
+          :src="logo"
+          :alt="name"
+        >
+        <span>{{ name }}</span>
       </VToolbarTitle>
-      <VSpacer />
-      <VToolbarSideIcon @click="navBar()" />
+      <!--<VSpacer />-->
+      <!--<VToolbarSideIcon @click="navBar()" />-->
     </VToolbar>
+    
+    
+    
+    
     <VList>
-      <VCalendar v-if="loggedIn && isBusinessCard" />
+      <VCalendar />
       <VListTile
-        v-for="item in items"
+        v-for="item in menu"
         :key="item.title"
-        :to="item.route"
+        :to="item.route.name"
       >
-        <VListTileAction>
-          <VIcon>{{ item.icon }}</VIcon>
-        </VListTileAction>
-
         <VListTileContent>
           <VListTileTitle>{{ item.title }}</VListTileTitle>
         </VListTileContent>
+        <VListTileAction>
+          {{ item.count }}
+        </VListTileAction>
+        <div
+          v-if="$route.path === '/' + item.route.name"
+          class="add-btn"
+        >
+          <v-icon>
+            add
+          </v-icon>
+        </div>
       </VListTile>
     </VList>
   </VNavigationDrawer>
@@ -44,16 +61,47 @@ export default {
   components: { VCalendar },
   data() {
     return {
-      business: {}
+      business: {},
+      name: 'Business Name',
+      logo: 'https://lh3.googleusercontent.com/Ax2wQYxjDITuZEpc6K9EDYPG7C839tb4PApia4Tmf18u8XehB-twqhVgDVPgxxExkr4=s180',
+      menu: [
+        {
+          title: 'Сотрудники',
+          count: '12',
+          route: { name: 'businessCardEmployee', id: this.$route.params.id }
+        },
+        {
+          title: 'Услуги',
+          count: '12',
+          route: { name: 'businessCardService', id: this.$route.params.id }
+        },
+        {
+          title: 'Клиенты',
+          count: '12',
+          route: { name: 'businessCardClients', id: this.$route.params.id }
+        },
+        {
+          title: 'Филиалы',
+          count: '12',
+          route: { name: 'businessCardFilal', id: this.$route.params.id }
+        },
+        {
+          title: 'Информация',
+          count: '12',
+          route: { name: 'businessCard', id: this.$route.params.id }
+        },
+        {
+          title: 'Галерея',
+          count: '12',
+          route: { name: 'businessCardGallery', id: this.$route.params.id }
+        }
+      ]
     };
   },
   computed: {
     ...mapGetters(['appTitle', 'loggedIn', 'token', 'navBarVisible']),
     businessId() {
       return this.$route && this.$route.params && this.$route.params.id;
-    },
-    items() {
-      return this.menu.filter(x => x.show);
     },
     isBusinessCard() {
       const businessCards = [
@@ -74,60 +122,6 @@ export default {
     isManagerMenu() {
       return !this.isBusinessCard;
     },
-    menu() {
-      return [
-        {
-          title: 'Мой профиль',
-          show: this.loggedIn && this.isManagerMenu
-        },
-        {
-          title: 'Сообщения',
-          show: this.loggedIn && this.isManagerMenu
-        },
-        {
-          title: 'Мои компании',
-          icon: 'business',
-          route: { name: 'myBusinessList' },
-          show: this.loggedIn && this.isManagerMenu
-        },
-        {
-          title: 'Все компании',
-          icon: 'business',
-          route: { name: 'businessList' },
-          show: !this.loggedIn || this.isManagerMenu
-        },
-        {
-          title: 'Информация',
-          show: this.loggedIn && this.isBusinessCard,
-          route: { name: 'businessCard', id: this.$route.params.id }
-        },
-        {
-          title: 'Услуги',
-          show: this.loggedIn && this.isBusinessCard,
-          route: { name: 'businessCardService', id: this.$route.params.id }
-        },
-        {
-          title: 'Сотрудники',
-          show: this.loggedIn && this.isBusinessCard && this.isCompany,
-          route: { name: 'businessCardEmployee', id: this.$route.params.id }
-        },
-        {
-          title: 'Клиенты',
-          show: this.loggedIn && this.isBusinessCard && this.isCompany,
-          route: { name: 'businessCardClients', id: this.$route.params.id }
-        },
-        {
-          title: 'Филиалы',
-          show: this.loggedIn && this.isBusinessCard && this.isCompany,
-          route: { name: 'businessCardFilal', id: this.$route.params.id }
-        },
-        {
-          title: 'Галерея',
-          show: this.loggedIn && this.isBusinessCard,
-          route: { name: 'businessCardGallery', id: this.$route.params.id }
-        }
-      ];
-    }
   },
   watch: {
     token: 'loadUserInfo',
@@ -157,3 +151,33 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+  .v-navigation-drawer>.v-list:not(.v-list--dense) .v-list__tile {
+    padding-right: 0;
+  }
+  .v-list__tile {
+    padding-right: 0!important;
+  }
+  .add-btn {
+    width: 40px;
+    height: 100%;
+    background: #EF4D37;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 10px;
+  }
+  .v-toolbar__content {
+  }
+  .v-toolbar__title {
+    display: flex;
+    padding-left: 16px;
+    align-items: center;
+  }
+  .logo {
+    margin-right: 10px;
+    max-width: 31px;
+    max-height: 31px;
+  }
+</style>
