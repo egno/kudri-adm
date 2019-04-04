fdd407d7f15a96c50c58047ee69d25deae9c942b
Egno <ex.lxndr@gmail.com>
2019-04-04 10:33:46 +0700

    mod deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ npm i && \
 npm run build && \
 rm -rf ../html/* && \
 cp -r dist/* ../html && \
-git log | head -n 3 | sed 's/$/<br>/g' > ../html/version.html && \
+git log | head -n 6 | sed 's/$/<br>/g' > ../html/version.html && \
 echo "Deployed successfully"
 
