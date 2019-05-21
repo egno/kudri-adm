3d9be17003dade74b9f51b889014e3d64b9ead5c
Egno <ex.lxndr@gmail.com>
2019-05-21 13:33:18 +0700

    #3440 go to login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,62 +1,64 @@
 <template>
-  <VCard v-if="loggedIn">
-    <VCardTitle primary-title>
-      <div>
-        <h3 class="headline mb-0">
-          Выйти из личного кабинета?
-        </h3>
+  <div>
+    <VCard v-if="loaded && loggedIn===true">
+      <VCardTitle primary-title>
         <div>
-          Сейчас вы авторизованы как
-          <span class="font-weight-medium">
-            {{ userID }}
-          </span>
+          <h3 class="headline mb-0">
+            Выйти из личного кабинета?
+          </h3>
+          <div>
+            Сейчас вы авторизованы как
+            <span class="font-weight-medium">
+              {{ userID }}
+            </span>
+          </div>
         </div>
-      </div>
-    </VCardTitle>
-    <VCardActions>
-      <VSpacer />
-      <VBtn
-        color="primary"
-        @click="sendLogout"
-      >
-        Выйти
-      </VBtn>
-    </VCardActions>
-  </VCard>
-  <div v-else>
-    <VForm class="businesscard-form _login">
-      <VTextField
-        v-model="flogin"
-        name="username"
-        label="Логин"
-        type="text"
-        browser-autocomplete="username"
-        class="businesscard-form__field"
-        :rules="[ rules.required ]"
-      />
-      <VTextField
-        id="password"
-        v-model="fpassword"
-        name="password"
-        label="Пароль"
-        type="password"
-        browser-autocomplete="current-password"
-        class="businesscard-form__field"
-        :rules="[ rules.required ]"
-      />
-      <div>
-        <a @click="goRestorePassword">
-          Забыли пароль?
-        </a>
-      </div>
-      <MainButton
-        type="button"
-        :class="['button', 'save-info', { button_disabled: !flogin || !fpassword }]"
-        @click.native.stop="sendLogin"
-      >
-        Вход
-      </MainButton>
-    </VForm>
+      </VCardTitle>
+      <VCardActions>
+        <VSpacer />
+        <VBtn
+          color="primary"
+          @click="sendLogout"
+        >
+          Выйти
+        </VBtn>
+      </VCardActions>
+    </VCard>
+    <div v-if="loaded && loggedIn===false">
+      <VForm class="businesscard-form _login">
+        <VTextField
+          v-model="flogin"
+          name="username"
+          label="Логин"
+          type="text"
+          browser-autocomplete="username"
+          class="businesscard-form__field"
+          :rules="[ rules.required ]"
+        />
+        <VTextField
+          id="password"
+          v-model="fpassword"
+          name="password"
+          label="Пароль"
+          type="password"
+          browser-autocomplete="current-password"
+          class="businesscard-form__field"
+          :rules="[ rules.required ]"
+        />
+        <div>
+          <a @click="goRestorePassword">
+            Забыли пароль?
+          </a>
+        </div>
+        <MainButton
+          type="button"
+          :class="['button', 'save-info', { button_disabled: !flogin || !fpassword }]"
+          @click.native.stop="sendLogin"
+        >
+          Вход
+        </MainButton>
+      </VForm>
+    </div>
   </div>
 </template>
 
@@ -80,9 +82,13 @@ export default {
     rules: {
       required: value => !!value || 'Это поле обязательно для заполнения',
     },
+    
   }),
   computed: {
-    ...mapGetters(['loggedIn', 'userID', 'userInfo'])
+    ...mapGetters(['loggedIn', 'userID', 'userInfo']),
+    loaded () {
+      return this.userInfo !== undefined
+    }
   },
   watch: {
     loggedIn (newVal, oldVal) {
