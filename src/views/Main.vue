5fd27778e017c6be205dc876ffea444813267d08
Natalya Norboeva <divonata@gmail.com>
2019-05-21 11:51:57 +0700

    #3433 redirect from main to login page

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,79 +1,17 @@
 <template>
-  <div>UNO</div>
+  <div />
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import Api from '@/api/backend'
 
 export default {
-  data () {
-    return {
-      businessCount: 0
-    }
-  },
-  computed: {
-    ...mapGetters(['userInfo','userRole']),
-    userId () {
-      return this.userInfo && this.userInfo.id
-    }
-  },
-  watch: {
-    userId: 'loadBusiness'
-  },
   mounted () {
-    this.loadBusiness()
+    this.$router.push({
+      name: 'login'
+    })
   },
   methods: {
-    loadBusiness () {
-      if (!this.userId) return
 
-      const this_ = this
-
-      Api()
-        .get(`my_business`)
-        .then(res => res.data)
-        .then(res => {
-          this_.businessCount = res.length
-
-          if (this_.userRole === 'manager' || this_.userRole === 'admin') {
-            this_.$router.push({
-              name: 'myBusinessList'
-            })
-            return
-          }
-
-          const company = res.find(business => business.type === 'C')
-          const filial = res.find(business => !!business.parent)
-
-          // if user has access to 1 company with no branches
-          if (this_.businessCount === 1 && res[0].id && !company && !filial) {
-            this_.$router.push({
-              name: 'businessCard',
-              params: { id: res[0].id }
-            })
-            return
-          }
-
-          // if user has no access to a company
-          if (!company && filial && filial.id) {
-            this_.$router.push({
-              name: 'filialList',
-              params: { id: filial.parent }
-            })
-            return
-          }
-
-          // if user has access to a company
-          if (company) {
-            this_.$router.push({
-              name: 'businessCard',
-              params: { id: company.id }
-            })
-            return
-          }
-        })
-    }
   }
 }
 </script>
