4eadcd5148201ff7c72340c75c188e6d56c4557f
Natalya Norboeva <divonata@gmail.com>
2019-06-14 11:24:22 +0700

    #3414 Clients: fix phone number format

--- src/main.js
+++ src/main.js
@@ -19,10 +19,18 @@ Vue.use(Meta)
 Vue.use(vueSmoothScroll)
 Vue.filter('phoneFormat', function (value) {
   if (!value) return ''
-  return value.replace(
-    /(\d?)(\d{1,3})(\d{1,3})(\d{1,2})(\d{1,2})$/g,
-    '+$1 ($2) $3-$4-$5'
-  )
+  if (value.length === 10) {
+    return value.replace(
+      /(\d{1,3})(\d{1,3})(\d{1,2})(\d{1,2})$/g,
+      '+7 ($1) $2-$3-$4'
+    )
+  } else {
+    return value.replace(
+      /(\d?)(\d{1,3})(\d{1,3})(\d{1,2})(\d{1,2})$/g,
+      '+$1 ($2) $3-$4-$5'
+    )
+  }
+
 })
 
 
