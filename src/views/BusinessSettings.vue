a2d752b452652da716bc0128ac18d23cb7cf4dda
Egno <ex.lxndr@gmail.com>
2019-07-09 16:37:13 +0700

    fix paymaster return path

--- src/views/BusinessSettings.vue
+++ src/views/BusinessSettings.vue
@@ -188,8 +188,8 @@ export default {
       const paymasterId = process.env.VUE_APP_PAYMASTER_ID
       const returnUrl = `${window.location.hostname}${window.location.pathname}`
       const url = `https://paymaster.ru/payment/init?LMI_MERCHANT_ID=${paymasterId}&LMI_PAYMENT_AMOUNT=${amount}&LMI_CURRENCY=643&LMI_PAYMENT_NO=${paymentNo}&LMI_PAYMENT_DESC=UNO_SALON&BUSINESS_ID=${this.businessId}&LMI_SUCCESS_URL=${encodeURIComponent(returnUrl)}`
-      console.log(url)
-      // window.location = url
+      // console.log(url)
+      window.location = url
     },
     load () {
       if (!this.businessId) return
