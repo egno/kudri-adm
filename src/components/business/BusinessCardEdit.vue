55921641f89ce63da46bfa92f95447980509c8bf
Egno <ex.lxndr@gmail.com>
2019-07-23 18:13:51 +0700

    fix avatar load

--- src/components/business/BusinessCardEdit.vue
+++ src/components/business/BusinessCardEdit.vue
@@ -251,6 +251,8 @@ import MainButton from '@/components/common/MainButton.vue'
 import Business from '@/classes/business'
 import BusinessSchedule from '@/classes/businessSchedule'
 import { debounce } from 'lodash'
+import { uuidv4 } from '@/components/utils'
+
 
 export default {
   components: {
@@ -477,8 +479,9 @@ export default {
       this.$emit('formChange')
     },
     onAvatarSave (img) {
-      this.data.j.avatar = ''
-      this.saveImage(img, this.data.id)
+      this.$set(this.data, 'j', {...this.data.j, ...{avatar: ''}})
+      const id = uuidv4()
+      this.saveImage(img, id)
         .then(() => {
           this.data.save().then(() => {
             this.setBusinessInfo(this.data)
